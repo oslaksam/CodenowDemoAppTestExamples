bd48a84a15ed8bf891305fbdb2c87d34c02103ca
sam <oslaksam@fit.cvut.cz>
2022-04-09 16:33:35 +0200

    Remake the first test successfully

--- CypressIODemo/cypress/integration/demoapp/demoapp.spec.js
+++ CypressIODemo/cypress/integration/demoapp/demoapp.spec.js
@@ -4,6 +4,7 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+const { faker } = require("@faker-js/faker");
 
 describe("demo reservation app", () => {
   beforeEach(() => {
@@ -11,45 +12,56 @@ describe("demo reservation app", () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit("http://localhost:3000/");
+    cy.visit("https://demo-app-fe.vercel.app/");
   });
-  it("creates the reservation", () => {
+  it("Creates a reservation", () => {
+    const name = faker.name.firstName();
+    const lastName = faker.name.lastName();
+    const email = faker.internet.email();
     cy.get(".rs-picker-toggle-value").click();
     cy.get(".rs-picker-toolbar-option:nth-child(1) > span").click();
     cy.get("[name='trainId']").click();
-    cy.get("div:nth-child(1) > .rs-picker-select-menu-item").click();
+    let val = Math.floor(Math.random() * (4 - 1 + 1) + 1);
+    cy.wait(500);
+    cy.get("div:nth-child(" + val + ") > .rs-picker-select-menu-item").click({
+      multiple: true,
+    });
+    val = Math.floor(Math.random() * (7 - 1 + 1) + 1);
+    cy.wait(500);
     cy.get("[name='seatId']").click();
-    cy.get("div:nth-child(1) > .rs-picker-select-menu-item").click();
+    cy.wait(500);
+    cy.get("div:nth-child(" + val + ") > .rs-picker-select-menu-item").click({
+      multiple: true,
+    });
     cy.get(".rs-form-group:nth-child(5) > .rs-input")
       .should("be.visible")
       .click()
-      .type("Tony");
+      .type(name);
     cy.get(".rs-form-group:nth-child(6) > .rs-input")
       .should("be.visible")
       .click()
-      .type("Bonny");
+      .type(lastName);
     cy.get(".rs-form-group:nth-child(7) > .rs-input")
       .should("be.visible")
       .click()
-      .type("bingus.tonis@example.com");
-    //const filepath =
-    // "C:UsersoslaksamDocumentsCODENOWSeleniumDemo\randomFile.bibtex";
-    //cy.get(".rs-form-group:nth-child(8) > .rs-input").attachFile(filepath);
-    // cy.get(".rs-form-group:nth-child(5) > .rs-input").should(
-    //   "have.text",
-    //   "Tony"
-    // );
-    // cy.get(".rs-form-group:nth-child(6) > .rs-input").should(
-    //   "have.text",
-    //   "Bonny"
-    // );
-    // cy.get(".rs-form-group:nth-child(7) > .rs-input").should(
-    //   "have.text",
-    //   '"bingus.tonis@example.com'
-    // );
+      .type(email);
+    const filepath = "example.json";
+    cy.get(".rs-form-group:nth-child(8) > .rs-input").attachFile(filepath);
+    cy.get(".rs-form-group:nth-child(5) > .rs-input").should(
+      "have.value",
+      name
+    );
+    cy.get(".rs-form-group:nth-child(6) > .rs-input").should(
+      "have.value",
+      lastName
+    );
+    cy.get(".rs-form-group:nth-child(7) > .rs-input").should(
+      "have.value",
+      email
+    );
     cy.get(".rs-btn-primary").should("be.visible").click();
-    cy.get(".rs-form").should("be.visible").submit();
-    cy.on("window:alert", (text) => {
+    //cy.get(".rs-form").should("be.visible").submit();
+    cy.on("window:confirm", (text) => {
       expect(text).to.contains("Reservation created successfully");
     });
   });
