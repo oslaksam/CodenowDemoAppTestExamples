cb0d0de8ca9dd7b1db2c1b171000b4021e6ca36e
sam <oslaksam@fit.cvut.cz>
2022-04-09 17:25:27 +0200

    update api

--- CypressIODemo/cypress/integration/demoapp/demoappApi.spec.js
+++ CypressIODemo/cypress/integration/demoapp/demoappApi.spec.js
@@ -4,53 +4,73 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+const { faker } = require("@faker-js/faker");
 
 describe("demo reservation app", () => {
-  beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
-    cy.visit("http://localhost:3000/");
+  context("GET /reservation", () => {
+    it("Should return a list of latest 50 reservations", () => {
+      cy.request({
+        method: "GET",
+        url: "https://demo-app-spring-api-aca-demo.stxcn-aca.stxcn.codenow.com/reservation",
+      }).should((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body[0]).to.have.all.keys(
+          "id",
+          "firstName",
+          "lastName",
+          "email",
+          "seatId",
+          "trainId",
+          "date",
+          "status"
+        );
+        cy.log(JSON.stringify(response.body));
+      });
+    });
   });
-  it("creates the reservation", () => {
-    cy.get(".rs-picker-toggle-value").click();
-    cy.get(".rs-picker-toolbar-option:nth-child(1) > span").click();
-    cy.get("[name='trainId']").click();
-    cy.get("div:nth-child(1) > .rs-picker-select-menu-item").click();
-    cy.get("[name='seatId']").click();
-    cy.get("div:nth-child(1) > .rs-picker-select-menu-item").click();
-    cy.get(".rs-form-group:nth-child(5) > .rs-input")
-      .should("be.visible")
-      .click()
-      .type("Tony");
-    cy.get(".rs-form-group:nth-child(6) > .rs-input")
-      .should("be.visible")
-      .click()
-      .type("Bonny");
-    cy.get(".rs-form-group:nth-child(7) > .rs-input")
-      .should("be.visible")
-      .click()
-      .type("bingus.tonis@example.com");
-    //const filepath =
-    // "C:UsersoslaksamDocumentsCODENOWSeleniumDemo\randomFile.bibtex";
-    //cy.get(".rs-form-group:nth-child(8) > .rs-input").attachFile(filepath);
-    // cy.get(".rs-form-group:nth-child(5) > .rs-input").should(
-    //   "have.text",
-    //   "Tony"
-    // );
-    // cy.get(".rs-form-group:nth-child(6) > .rs-input").should(
-    //   "have.text",
-    //   "Bonny"
-    // );
-    // cy.get(".rs-form-group:nth-child(7) > .rs-input").should(
-    //   "have.text",
-    //   '"bingus.tonis@example.com'
-    // );
-    cy.get(".rs-btn-primary").should("be.visible").click();
-    cy.get(".rs-form").should("be.visible").submit();
-    cy.on("window:alert", (text) => {
-      expect(text).to.contains("Reservation created successfully");
+  context("POST /reservation", () => {
+    it("Should create a reservation", () => {
+      cy.request(
+        {
+          method: "POST",
+          url: "https://demo-app-spring-api-aca-demo.stxcn-aca.stxcn.codenow.com/reservation",
+        },
+        {
+          date: "2022-03-21T02:24:59.815Z",
+          email: faker.internet.email(),
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          seatId: "1-21",
+          trainId: "ICE-575",
+        }
+      ).should((response) => {
+        expect(response.status).to.eq(200);
+
+        cy.log(JSON.stringify(response.body));
+      });
+    });
+    it("Should fail to create a reservation", () => {
+      cy.request(
+        {
+          method: "POST",
+          url: "https://demo-app-spring-api-aca-demo.stxcn-aca.stxcn.codenow.com/reservation",
+        },
+        {}
+      ).should((response) => {
+        expect(response.status).to.eq(500);
+        cy.log(JSON.stringify(response.body));
+      });
+    });
+  });
+  context("PUT /reservation", () => {
+    it("Should cancel a reservation", () => {
+      cy.request({
+        method: "PUT",
+        url: "https://demo-app-spring-api-aca-demo.stxcn-aca.stxcn.codenow.com//reservation/cancel/0",
+      }).should((response) => {
+        expect(response.status).to.eq(200);
+        cy.log(JSON.stringify(response.body));
+      });
     });
   });
 });
